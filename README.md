5ef761efce51d01e53890c655da8c5fa2f6d1cf9
gluchy1 <m.slonowski@poczta.fm>
2023-10-20 02:42:11 +0200

    button driver op.

--- README.md
+++ README.md
@@ -1,52 +1,29 @@
-# Job Offer Scraping tool
+# Job Offer Scraping tool + MongoDB integration
 
 ### Note:
-To modify the code in order to make it use a different URL (not tried yet), please modify the `driver.get("your url")` parameter. I suggest pasting the URL with already chosen requirements, such as Job Title, Localisation etc.
+To modify the code in order to make it use a different URL just edit config.env, but it is strongly adapted for pracuj.pl site. I suggest pasting the URL with already chosen requirements, such as Job Title, Localisation etc. -> not to over deliver useless data
 
 ## Written in Python 3.10 using Selenium, Pandas and Spacy
 
-This code is designed to scrape job offers from the website [pracuj.pl](https://www.pracuj.pl/) related to Python programming in Warsaw. It utilizes the Selenium WebDriver along with the Chrome browser to navigate the website and extract job details.
+This is a Python script that automates the task of collecting job offers from a specific website (in this case [pracuj.pl](https://www.pracuj.pl/)). It not only grabs basic details like job titles and company names but also extracts job requirements. All collected information is stored in a MongoDB database.
+
+After running for a while, it sorts and prints all the reuqirements found, from the most common to the rarest:
+
 
 ![image](https://github.com/gluchy1/joboffer-scraping-tool/assets/70800019/331cf67e-a110-432d-bbae-b16c38d88225)
 
-![image](https://github.com/gluchy1/joboffer-scraping-tool/assets/70800019/5a4d8fa4-7ee8-495e-a54e-2b2cbf2e5c64)
+## **Libraries Used:**  
 
-Here's an overview of the code:  
+- **Selenium:** Clicks around the web page for us.
+- **BeautifulSoup:** Reads the web page's code to pick out the details we want.
+- **spaCy:** Looks through the job description to find important keywords.
+- **MongoDB:** This is where we keep all the job data we collect.
 
-- The code begins by importing necessary modules: `os`, `webdriver` from Selenium, `BeautifulSoup` from bs4, `pandas`, `spacy`, and `Counter` from collections.
+### **Some editable variables for future adaptation to other websites**
 
-- The project directory path is obtained using `os.path.dirname()` and `os.path.abspath(__file__)`. This allows the code to locate the `chromedriver.exe` file within the project directory dynamically.
-
-- Chrome options are set using `Options()` from the `selenium.webdriver.chrome.options` module. Two options, `--no-sandbox` and `--disable-dev-shm-usage`, are added to ensure a smooth execution.
-
-- The Selenium WebDriver is set up using `Service()` and `webdriver.Chrome()`, passing the `chrome_driver_path` and `chrome_options`.
-
-- The WebDriver navigates to the target URL "[pracuj.pl](https://www.pracuj.pl/praca/programista%20python;kw/warszawa;wp?rd=30&et=1%2C17)" and accepts the cookie consent by finding the corresponding button element using XPath.
-
-- The code loads spaCy models for English and Polish language processing using `spacy.load()`.
-
-- The `extract_requirements()` function takes a text parameter, uses spaCy to extract nouns and proper nouns, and returns a list of requirements.
-
-- A list `job_offers_list` is created to store the scraped job offer data.
-
-- The code finds all job offers on the page by locating elements using XPath.
-
-- For each job offer, a new tab is opened using `driver.execute_script("window.open('');")` and switched to using `driver.switch_to.window()`.
-
-- The offer's link is accessed, and the code tries to extract the job title and company name from the page. If the strings "O firmie" or "About the company" are present in the titles, they are removed.
-
-- The code attempts to scrape the job requirements from the offer page. It uses `BeautifulSoup` to parse the HTML and find the requirements list. If found, it iterates over the requirements and appends them to the `requirements` list.
-
-- If no requirements are found using the previous method, the code tries to extract them from the job description by calling the `extract_requirements()` function.
-
-- The job offer data, including the link, job title, company name, and requirements, is stored in a dictionary and appended to the `job_offers_list`.
-
-- After scraping all job offers, the current tab is closed, and the driver is switched back to the main window.
-
-- The collected job offer data is converted into a pandas DataFrame and saved as an Excel file named "jobOffers.xlsx" using `df.to_excel()`.
-
-- Finally, the driver is quit to ensure the browser window is closed.
-
-- The `print_sorted_requirements()` function reads the Excel file, extracts the requirements column, combines them into a single list, counts the occurrences of each requirement using `Counter`, and prints the sorted requirements along with their counts.
+        job_title = driver.find_element(By.XPATH, "//h1[@data-scroll-id='job-title']").text
+        company_name_element = driver.find_element(By.XPATH, "//h2[@data-scroll-id='employer-name']")
+        company_name = company_name_element.text.split("\n")[0]
+        company_name = company_name.replace("About the company", "").replace("O firmie", "").strip()
 
 **Note:** Make sure to update the URL and adjust the code if needed to match the target website structure or specific requirements.
--- main.py
+++ main.py
@@ -1,126 +1,186 @@
+"""
+Job Scraper
+
+This script is designed to scrape job offer information from a given website.
+
+It uses Selenium, BS4, Spacy and other libraries alongside MongoDB integration.
+
+~ ~ keep updating chromedriver.exe if code doesn't run.
+
+"""
+
 import os
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import pandas as pd
-import spacy
 from collections import Counter
+from typing import List
 
-'''
-In order to make it more versatile, I've added lines of code that make program able to access the
-"chromedriver.exe" without making it necessary to modify the path of the file for each unique user that
-downloads this code from repo.
-'''
+# DS
+import spacy
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-chrome_driver_path = os.path.join(project_dir, "chromedriver.exe")
+# BS
+from bs4 import BeautifulSoup
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+# mongo
+from pymongo import MongoClient
 
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.maximize_window()
+# selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-driver.get("https://www.pracuj.pl/praca/programista%20python;kw/warszawa;wp?rd=30&et=1%2C17")
-driver.switch_to.window(driver.window_handles[0])
-driver.find_element(By.XPATH, "//button class[contains(., 'Akceptuj wszystkie')]").click()
+# .env
+from dotenv import load_dotenv
 
-nlp = spacy.load("en_core_web_sm")
-nlp_pl = spacy.load("pl_core_news_sm")
+# enviromental variable
+load_dotenv(dotenv_path="config.env")
 
-def extract_requirements(text):
-    doc = nlp(text)
-    requirements = []
-    for sent in doc.sents:
-        for token in sent:
-            if token.pos_ == "NOUN" or token.pos_ == "PROPN":
-                requirements.append(token.text)
 
-    doc_pl = nlp_pl(text)
-    for sent in doc_pl.sents:
-        for token in sent:
-            if token.pos_ == "NOUN" or token.pos_ == "PROPN":
-                requirements.append(token.text)
+# main class
+def scrape_page(driver: webdriver.Chrome, page_number: int):
+    # Get scrape URL from environment variable
+    base_url = os.getenv("SCRAPE_URL")
+    url = f"{base_url}{page_number}"
+    driver.get(url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//a[@data-test='link-offer']"))
+    )
 
-    return requirements
 
-job_offers_list = []
-
-job_offers = driver.find_elements(By.XPATH, "//a[@data-test='link-offer']")
-for offer in job_offers:
-    job_link_href = offer.get_attribute("href")
-
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get(job_link_href)
-
+def get_job_details(driver: webdriver.Chrome) -> (str, str):
     try:
-        job_title_element = driver.find_element(By.XPATH, "//h1[@data-scroll-id='job-title']")
-        job_title = job_title_element.text
-        if "O firmie" in job_title:
-            job_title = job_title.replace("O firmie", "").strip()
-        elif "About the company" in job_title:
-            job_title = job_title.replace("About the company", "").strip()
+        job_title = driver.find_element(By.XPATH, "//h1[@data-scroll-id='job-title']").text
     except:
         job_title = "N/A"
 
     try:
         company_name_element = driver.find_element(By.XPATH, "//h2[@data-scroll-id='employer-name']")
         company_name = company_name_element.text.split("\n")[0]
-        if "O firmie" in company_name:
-            company_name = company_name.replace("O firmie", "").strip()
-        elif "About the company" in company_name:
-            company_name = company_name.replace("About the company", "").strip()
+        company_name = company_name.replace("About the company", "").replace("O firmie", "").strip()
     except:
         company_name = "N/A"
 
-    requirements = []
-    try:
+    return job_title, company_name
+
+
+def get_webdriver() -> webdriver.Chrome:
+    """Sets up and returns a Selenium WebDriver."""
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    chrome_driver_path = os.path.join(project_dir, "chromedriver.exe")
+
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    service = Service(chrome_driver_path)
+
+    return webdriver.Chrome(service=service, options=chrome_options)
+
+
+class JobScraper:
+    def __init__(self):
+        self.nlp_pl = None
+        self.nlp = None
+        self.collection = None
+        self.db = None
+        self.client = None
+        self.initialize_database()
+        self.initialize_nlp()
+
+    # used for connection to MongoDB
+    def initialize_database(self):
+        # Get MongoDB URL from environment variable
+        mongo_url = os.getenv("MONGO_URL") # gets it from config.env
+        try:
+            self.client = MongoClient(mongo_url)
+            self.db = self.client['job_offers_db'] # name of the DB
+            self.collection = self.db['job_offers'] # name of the collection
+        except Exception as e:
+            print(f"Failed to connect to database. Error: {e}")
+
+    # spaCy usage for nlp
+    def initialize_nlp(self):
+        self.nlp = spacy.load("en_core_web_sm")
+        self.nlp_pl = spacy.load("pl_core_news_sm")
+
+    # spaCy usage for nlp
+    def extract_requirements(self, text: str) -> List[str]:
+        # Extracts job requirements from given string using NLP.
+        return self.extract_keywords(self.nlp, text) + self.extract_keywords(self.nlp_pl, text)
+
+    # spaCy usage for nlp
+    @staticmethod
+    def extract_keywords(nlp_engine, text: str) -> List[str]:
+        doc = nlp_engine(text)
+        return [token.text for sent in doc.sents for token in sent if token.pos_ in ["NOUN", "PROPN"]]
+
+    # used with process_offer
+    def scrape_and_store(self):  # main fuction for scraping
+        with get_webdriver() as driver:
+            for page_number in range(1, 11):  # change the range as needed
+                scrape_page(driver, page_number)
+                offers = driver.find_elements(By.XPATH, "//a[@data-test='link-offer']")
+                for offer in offers:
+                    self.process_offer(driver, offer)
+
+
+    # opens the new tab with a job offer, scrapes it and saves the data in the DB
+    def process_offer(self, driver: webdriver.Chrome, offer):  # function for processing data to DB
+        job_link_href = offer.get_attribute("href")
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get(job_link_href)
+
+        job_title, company_name = get_job_details(driver)
+        requirements = self.get_requirements(driver)
+
+        job_offer_data = {
+            'Link': job_link_href,
+            'Job Title': job_title,
+            'Company Name': company_name,
+            'Requirements': requirements
+        }
+
+        if not self.collection.find_one({'Link': job_link_href}):
+            self.collection.insert_one(job_offer_data)
+
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+
+    # gets requirements from the job offer
+    # if it fails to get requirements from the list, it searches for it in the job desc.
+    def get_requirements(self, driver: webdriver.Chrome) -> List[str]:
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         requirements_list = soup.find("ul", class_="offer-viewEX0Eq-")
-        if requirements_list:
-            requirements_items = requirements_list.find_all("p", class_="offer-viewU0gxPf")
-            for item in requirements_items:
-                requirements.append(item.text)
-    except:
-        pass
+        requirements = [
+            item.text for item in requirements_list.find_all("p", class_="offer-viewU0gxPf")
+        ] if requirements_list else []
 
-    if not requirements:
-        try:
-            job_description_element = driver.find_element(By.XPATH, "//div[@data-scroll-id='job-description']")
-            job_description = job_description_element.text
-            requirements = extract_requirements(job_description)
-        except:
-            pass
+        if not requirements:
+            try:
+                job_description = driver.find_element(By.XPATH, "//div[@data-scroll-id='job-description']").text
+            except:
+                job_description = ""
+            requirements = self.extract_requirements(job_description)
 
-    job_offer_data = {
-        'Link': job_link_href,
-        'Job Title': job_title,
-        'Company Name': company_name,
-        'Requirements': requirements
-    }
-    job_offers_list.append(job_offer_data)
+        return requirements
 
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+    # from the collected data, sorts and prints the requirements
+    def print_sorted_requirements(self):
+        all_requirements = [
+            req for job_offer in self.collection.find({}, {'Requirements': 1, '_id': 0})
+            for req in job_offer.get('Requirements', [])
+        ]
+        requirements_count = Counter(all_requirements)
+        sorted_requirements = sorted(requirements_count.items(), key=lambda x: x[1], reverse=True)
 
-df = pd.DataFrame(job_offers_list)
-df.to_excel('jobOffers.xlsx', index=False)
+        for requirement, count in sorted_requirements:
+            print(f'[{requirement}][Amount = {count}]')
 
-driver.quit()
 
-def print_sorted_requirements():
-    df = pd.read_excel('jobOffers.xlsx')
-    requirements_list = df['Requirements'].tolist()
-    all_requirements = [req for reqs in requirements_list for req in eval(reqs)]
-    requirements_count = Counter(all_requirements)
-    sorted_requirements = sorted(requirements_count.items(), key=lambda x: x[1], reverse=True)
-
-    for requirement, count in sorted_requirements:
-        print(f'[{requirement}][Amount = {count}]')
-
-print_sorted_requirements()
+# used for starting the whole process by initating the JobScraper and then running
+# scrape_and_store(), then prints the requirements.
+if __name__ == "__main__":
+    scraper = JobScraper()
+    scraper.scrape_and_store()
+    scraper.print_sorted_requirements()
